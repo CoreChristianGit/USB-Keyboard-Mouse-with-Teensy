43c4e8dbc59343715a0a9063a516af63a649bab8
CoreChristianGit <39609964+CoreChristianGit@users.noreply.github.com>
2018-07-11 16:22:21 +1000

    Added comments

--- 3D_mouse.ino
+++ 3D_mouse.ino
@@ -11,11 +11,16 @@
 #include <Bounce.h>
 
 //  Declare Pin Assignments
+  //  The linear slide pot is here
 const int potSlider = A2;
+  //  The x and y components from the joystick are here
 const int yPin = A1;
 const int xPin = A0;
+  //  The internal built LED is here
 const int ledPin = 13;
+  //  The click functionality of the joystick is here
 const int shiftPin = 12;
+  //  The buttons are here 
 const int buttonFour = 6;
 const int buttonThree = 5;
 const int buttonTwo = 4;
@@ -28,10 +33,15 @@ float xMovement = 0;
 float yMovement = 0;
 
 //  Define  Constants
+  //  The debounce library constants are here
 const int BUTTON_NUM = 4;
 const int DEBOUNCE_TIME = 5;
+  //  The normal (letter/number) keybinds are here for shift pressed (P) and
+  //  shift not pressed (NP)
 const int KEYBIND_SHIFT_P[BUTTON_NUM] = {KEY_S, KEY_Y, KEY_Z, KEY_DELETE};
 const int KEYBIND_SHIFT_NP[BUTTON_NUM] = {KEY_M, KEY_W, 0, KEY_U};
+  //  The modifier (shift, ctrl, etc.) keybinds are here for shift pressed (P)
+  //  and shift not pressed (NP)
 const int KEYBIND_MOD_SHIFT_P[BUTTON_NUM] = {MODIFIERKEY_CTRL, MODIFIERKEY_CTRL, MODIFIERKEY_CTRL, 0};
 const int KEYBIND_MOD_SHIFT_NP[BUTTON_NUM] = {0, MODIFIERKEY_CTRL, 0, MODIFIERKEY_CTRL}; 
 
@@ -42,6 +52,8 @@ Bounce buttonArray[BUTTON_NUM] = {
   Bounce(buttonThree, DEBOUNCE_TIME),
   Bounce(buttonFour, DEBOUNCE_TIME)
 };
+
+//  Create the joystick bounce instance
 Bounce shiftButton = Bounce(shiftPin, DEBOUNCE_TIME);
 
 void setup() {
@@ -76,13 +88,16 @@ void loop() {
   //  required state
   for(int i = 0; i < BUTTON_NUM; i++){
     if(buttonArray[i].fallingEdge()){
+      //  Check to see if the shiftState is NOT active 
       if(!shiftState){
+        //  Apply the normal state keybinds
         Keyboard.set_modifier(KEYBIND_MOD_SHIFT_P[i]);
         Keyboard.set_key1(KEYBIND_SHIFT_P[i]);
         Keyboard.send_now();
         Serial.println(KEYBIND_MOD_SHIFT_P[i]);
         Serial.println(KEYBIND_SHIFT_P[i]);
         }else{
+        //  Apply the shift state keybinds
         Keyboard.set_modifier(KEYBIND_MOD_SHIFT_NP[i]);
         Keyboard.set_key1(KEYBIND_SHIFT_NP[i]);
         Keyboard.send_now();
@@ -90,6 +105,7 @@ void loop() {
         Serial.println(KEYBIND_SHIFT_NP[i]);
       }
     }else if(buttonArray[i].risingEdge()){
+      //  If no buttons are pressed, send nothing
       Keyboard.set_modifier(0);
       Keyboard.set_key1(0);
       Keyboard.send_now();
@@ -97,7 +113,7 @@ void loop() {
     }
   }
 
-  //  For Orbit control
+  //  For Orbit control. This is different as it includes a middle mouse click
   if(buttonArray[2].fallingEdge()){
     Keyboard.set_modifier(MODIFIERKEY_SHIFT);
     Keyboard.send_now();
